9cd604d510dcb142e39ec0a1b54c6e8a1575cc8d
mumina04 <muminadalacha@icloud.com>
2024-09-21 21:52:21 +0300

    added new style

--- my-nextjs-app/app/forgot-password/page.tsx
+++ my-nextjs-app/app/forgot-password/page.tsx
@@ -32,8 +32,7 @@ export default function ForgotPassword() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg blue-100">
-        
+    <div className="flex flex-col items-center justify-center min-h-screen bg blue-100">  
       <main className="w-full max-w-sm p-8 bg-white shadow-md rounded-lg">
         <h1 className=" text-black text-2xltext  font-bold text-center mb-6">Forgot Password</h1>
         <form onSubmit={handleSubmit} className="flex flex-col gap-4">
